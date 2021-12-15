da29850d3054c5c964b4042e64201d7c63db3be9
NooRAl-Deen <noordwaikat6@gmail.com>
2021-12-16 00:38:46 +0200

    Add Noor Al Deen Functions

--- js/script.js
+++ js/script.js
@@ -1,3 +1,111 @@
+/* [NooR Al Deen] -> Start */
+
+// Create tasksArray And Initialize To Values Of LocalStorage If Exists
+// To Prevent Initialize Empty Array If There Tasks At LocalStorge
+// To Save Tasks
+let tasksArray = JSON.parse(localStorage.getItem("tasks"))
+  ? JSON.parse(localStorage.getItem("tasks"))
+  : [];
+  
+// Get The Input Field
+let inputText = document.querySelector(".addNewTask");
+
+// Add Event Listener On Input Field
+inputText.addEventListener("keypress", (e) => {
+  // If Enter Pressed Then ->
+  if (e.key == "Enter") {
+    if (inputText.value != "") {
+      // Create Task Contains inputText.value And Add It To tasksArray
+      addTasksToArray(inputText.value);
+      // Add tasksArray Element To the Page
+      addElementsToPage(tasksArray);
+    }
+    // Empty Input Field
+    inputText.value = "";
+  }
+});
+
+// Recall The Function To Show Elements In The Page After Reloading
+if (tasksArray.length > 0) {
+  addElementsToPage(tasksArray);
+}
+
+function addElementsToPage(tasksArray) {
+  let tasks = document.querySelector(".tasks");
+  // To Prevent Repeated Tasks When Reloading Page
+  tasks.innerHTML = "";
+  // Loop Through tasksArray And Create Element For Each Task
+  tasksArray.forEach((ts) => {
+    let task = document.createElement("div");
+    task.classList.add("task");
+    if (ts.isCompleted) {
+      task.classList.add("completed");
+    } else {
+      task.classList.add("active-task");
+    }
+    task.classList.add("flex-row");
+    task.setAttribute("data-id", `${ts.id}`);
+    task.setAttribute("draggable", true);
+    let checkBoxDiv = document.createElement("div");
+    checkBoxDiv.classList.add("checkbox");
+    let circleDiv = document.createElement("div");
+    circleDiv.classList.add("circle");
+    let iElement = document.createElement("i");
+    iElement.classList.add("fa");
+    iElement.classList.add("fa-check");
+    let taskContent = document.createElement("div");
+    taskContent.classList.add("task-content");
+    let pElement = document.createElement("p");
+    let textNode = document.createTextNode(ts.title);
+    pElement.appendChild(textNode);
+    let iElementCon = document.createElement("i");
+    iElementCon.classList.add("fa");
+    iElementCon.classList.add("fa-times");
+    iElementCon.classList.add("deleteBtn");
+    circleDiv.appendChild(iElement);
+    checkBoxDiv.appendChild(circleDiv);
+    task.appendChild(checkBoxDiv);
+    taskContent.appendChild(pElement);
+    taskContent.appendChild(iElementCon);
+    task.appendChild(taskContent);
+    tasks.appendChild(task);
+    checkBoxDiv.onclick = clickToComplete;
+  });
+}
+
+// Add Tasks To tasksArray
+function addTasksToArray(taskText) {
+  // Create Task Object
+  let task = {
+    id: Date.now(),
+    title: taskText,
+    isCompleted: false,
+  };
+  // Push Task To tasksArray
+  tasksArray.push(task);
+  // Add tasksArray To LocalStorage
+  localStorage.setItem("tasks", JSON.stringify(tasksArray));
+}
+
+function clickToComplete(e) {
+  e.currentTarget.parentElement.classList.toggle("completed");
+  e.currentTarget.parentElement.classList.toggle("active-task");
+  changeIsCompleted(e.currentTarget.parentElement.getAttribute("data-id"));
+}
+
+function changeIsCompleted(taskid) {
+  for (let i = 0; i < tasksArray.length; i++) {
+    if (tasksArray[i].id == taskid) {
+      tasksArray[i].isCompleted == false
+        ? (tasksArray[i].isCompleted = true)
+        : (tasksArray[i].isCompleted = false);
+      localStorage.setItem("tasks", JSON.stringify(tasksArray));
+    }
+  }
+}
+
+/* [NooR Al Deen] -> End */
+
 /* DRAG AND DROP .. 'LEEN'  */
 const dragArea = document.querySelector(".tasks");
 new Sortable(dragArea, {
